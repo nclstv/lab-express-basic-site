1962a6a230aba71ac0ffc6874c8f1132cd59638c
Nicolas Stevens <stvns.ncls@gmail.com>
2023-07-03 16:50:24 +0200

    100% lab

---
+++ app.js
@@ -0,0 +1,19 @@
+const express = require('express')
+const app = express()
+
+app.use(express.static('public'))
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/views/home.html')
+})
+
+app.get('/about', (req, res) => {
+    res.sendFile(__dirname + '/views/about.html')
+})
+
+app.get('/works', (req, res) => {
+    res.sendFile(__dirname + '/views/works.html')
+})
+
+
+app.listen(3000, () => {console.log('Server is running on port: 3000')})
